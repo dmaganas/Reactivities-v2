1148bf258ac40a68a508c5b1d2e8bcd9a194e09e
dmagana <dmagana@apexsystems.com>
2025-08-04 10:46:07 -0600

    End of section #4

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,8 @@
+using Application.Activities.Queries;
+using Application.Core;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.VisualBasic;
+using Microsoft.Extensions.DependencyInjection;
+using AutoMapper;
 using Persistance;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +14,9 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
+
+builder.Services.AddMediatR(x => x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>());
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
 var app = builder.Build();
 
